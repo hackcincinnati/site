2c895e4b6560e7d1dda8e6a49731fad564587bb1
Dalton Craven <20dalton00@gmail.com>
2019-01-20 12:43:21 -0600

    add social plug

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import styled from 'styled-components'
+
+const StyledA = styled.a`
+  font-weight: bold;
+  color: black;
+
+  :hover {
+    color: black;
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -28,6 +38,14 @@ const IndexPage = () => (
       Never coded before? Don’t worry! We’ve got workshops to help new coders
       that will jump-start your first project. With newcomers and veterans
       alike, Hack Cincinnati is a surefire way to create something amazing.
+    </p>
+    <h3>Want to learn more?</h3>
+    <p>
+      Follow us on{' '}
+      <StyledA href="https://facebook.com/hackcincinnati">Facebook</StyledA>,{' '}
+      <StyledA href="https://instagram.com/hackcincinnati">Instagram</StyledA>,
+      or <StyledA href="https://twitter.com/hackcincinnati">Twitter</StyledA>{' '}
+      for more details and pre-registration updates!
     </p>
   </Layout>
 )
