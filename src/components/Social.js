bd004b8dc54b96efe788236b9a40419d294b1a75
Dalton Craven <20dalton00@gmail.com>
2019-01-29 08:46:58 -0500

    add social splash

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,24 +2,26 @@ import React from 'react'
 import { StyledOutboundLink, Container, Color } from '../components/Theme'
 
 const Social = () => (
-  <Container>
-    <h3>Want to learn more?</h3>
-    <p>
-      Follow us on{' '}
-      <StyledOutboundLink href="https://facebook.com/hackcincinnati">
-        Facebook
-      </StyledOutboundLink>
-      ,{' '}
-      <StyledOutboundLink href="https://instagram.com/hackcincinnati">
-        Instagram
-      </StyledOutboundLink>
-      , or{' '}
-      <StyledOutboundLink href="https://twitter.com/hackcincinnati">
-        Twitter
-      </StyledOutboundLink>{' '}
-      for more details and pre-registration updates!
-    </p>
-  </Container>
+  <Color>
+    <Container>
+      <h3>Want to learn more?</h3>
+      <p>
+        Follow us on{' '}
+        <StyledOutboundLink href="https://facebook.com/hackcincinnati">
+          Facebook
+        </StyledOutboundLink>
+        ,{' '}
+        <StyledOutboundLink href="https://instagram.com/hackcincinnati">
+          Instagram
+        </StyledOutboundLink>
+        , or{' '}
+        <StyledOutboundLink href="https://twitter.com/hackcincinnati">
+          Twitter
+        </StyledOutboundLink>{' '}
+        for more details and pre-registration updates!
+      </p>
+    </Container>
+  </Color>
 )
 
 export default Social
